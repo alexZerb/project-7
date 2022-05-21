7ad037345fb333bb3898cf3743efd80712221d41
Alex Szczerba <Szczerba18@icloud.com>
2022-05-21 12:28:19 -0700

    major change
    attempting to rewrite app.js as a function, had to components back into app.js but will restructure once data is being requested correctly

--- react-gallery-app/src/App.js
+++ react-gallery-app/src/App.js
@@ -1,45 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React  from 'react';
 import axios from 'axios';
 import './App.css';
-import apiKey from './config.js';
+import apiKey from './config'
 
-import Nav from './components/Nav';
-import SearchForm from './components/SearchForm';
-import PhotoContainer from './components/PhotoContainer'
 export default function App() {
-  const query = useState('');
   
-
-
-  const newSearch = (query = 'lions') => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-    .then( response => {
-      this.setState({
-        pics: response.data.photos.photo,
-        query: query,
+  const newSearch = ( query = 'lions') => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&format=json&nojsoncallback=1`)
+    .then( res => {
+      const data = res.data;
+      console.log(data);
     })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });
-  })
   }
+  newSearch();
+  return(
+    <div className='main-container'>
+      <form className='search-form'>
+        <input type='search' name='search' placeholder='Search' required></input>
+        <button type="submit" class="search-button">
+          <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+        </button>
+      </form>
+      <nav className='main-nav'>
+        <ul>
+          <li>Cats</li>
+          <li>Dogs</li>
+          <li>Computers</li>          
+        </ul>
+      </nav>
 
-  useEffect()
-
-  const handleClick = e => {
-    const query = e.target.id;
-    newSearch(query);
-  }
-
-    return (
-      <div className='container'>
-        <SearchForm />
-        <Nav navItem={handleClick} />
-        <PhotoContainer data={this.state.photos} title={this.state.query} altTag={this.state.query}/>
+      <div className='photo-container'>
+        <h2>I'm really trying to make this work</h2>
+        <ul>
+          <li>
+            <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
+          </li>
+          <li>
+            <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
+          </li>
+          <li>
+            <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
+          </li>
+          <li>
+            <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
+          </li>
+          {/* {switch to not found if problem getting links}  */}
+          <li class="not-found">
+            <h3>No Results Found</h3>
+            <p>You search did not return any results. Please try again.</p>
+          </li>
+        </ul>
       </div>
-    );
-  
-  
+    </div>
+  )
 }
 
 
--- react-gallery-app/src/index.js
+++ react-gallery-app/src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
     
 );
